6521299118fe01d42ec5c9e6ddc17472ec3bd8d8
kumar <obulesu.1406@gmail.com>
2024-12-31 05:24:35 -0500

    Added few more scripts

---
+++ 05-VarHiding.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter the USERNAME::" #test entered here will abe added as value to variable"
+read USERNAME #Here USERNAME is variable Name
+
+echo "username entered: $USERNAME"
+
+echo "Please enter the Password::"
+read -s PASSWORD #entering the password and -s is for hiding input that we entered
+
