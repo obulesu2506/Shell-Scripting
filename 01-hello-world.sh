a112f8d83455e52ee156fc5cd5d0065ccb67c223
Obulesu <obulesu.1406@gmail.com>
2025-01-09 14:34:22 +0530

    New comment added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Shebang is used as a Interpreter for validating the syntax and data to smooth execution
+
+
 echo "Hello World"
 echo "How are you doing"
 echo "I am started to learn Shell Scripting"
