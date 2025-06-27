337aa4de26465fa4b79e0c675f87d8f100f479ef
obulesu <obulesu.1406@gmail.com>
2025-06-27 06:39:55 -0400

    Realtime Practice

--- RealtimeUseCases/01-backupscript.sh
+++ RealtimeUseCases/01-backupscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-backup_dir = "/tmp/backup"
-source_dir = "/var/log/messages"
+backup_dir="/tmp/backup"
+source_dir="/var/log/messages"
 
 #Create backup file with timestamp
 tar -czf "$backup_dir/backup_$(date +%Y%m%d-%H%M%S).tar.gz" "$source_dir"
